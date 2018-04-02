ef6c7f1777fcbc39ed4183dbb85b9aff40cd8e00
Mikias Tilahun Abebe <mikiastilahun@gmail.com>
2018-04-02 22:50:39 +0300

    fixed the admins

---
+++ database/migrations/2018_04_02_193432_theadmins.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Theadmins extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('theadmins', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('avatar')->default('/theadmin/default.jpg');
+            $table->string('user_id')->unique();
+            $table->string('email')->unique();
+            $table->integer('phone')->unique();
+            $table->string('password');
+            $table->string('pin');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('theadmins');
+    }
+}
